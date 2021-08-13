40ce1ae5563a68c47e8f1e854387a5fce1328793
serega <sergey.gutovsk@gmail.com>
2021-08-13 22:04:08 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const minicss = require('mini-css-extract-plugin'); 
+
+module.exports = {
+	mode: 'development',
+	entry: {
+        app: './resources/js/app.js',
+    },
+	output: {
+		path: path.resolve(__dirname, 'public'),
+		filename: '[name]-bundle.js',
+	},
+    module: {
+        rules: [
+            {
+                test:/\.(s*)css$/,
+                use: [
+                    minicss.loader,
+                    { loader: 'css-loader', options: { url: false } },
+                    { loader: 'sass-loader', options: { sourceMap: true } }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new minicss({
+            filename: '[name]-bundle.css',
+        }),
+    ]
+};
